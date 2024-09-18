c1a83ac6760e311a8eebb137051bb10eaed3592c
Girish0744 <girishbhuteja07@gmail.com>
2024-09-18 18:01:27 -0400

    Step 3 Done: The file StudentData.txt can be opened, Parsed the data, created an object and pushed that object into a vector space.

--- Assignment 2 Project 5/Source.cpp
+++ Assignment 2 Project 5/Source.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+// Structure for the student data
+struct STUDENT_DATA {
+    string First_Name;
+    string Last_Name;
+};
 
 int main() {
-    return 1;
+    // Opening the file StudentData.txt
+    ifstream file("StudentData.txt");
+    if (!file.is_open()) {
+        cerr << "The file StudentData.txt cannot be opened.\n";
+        return 1;
+    }
+
+    vector<STUDENT_DATA> Students;
+    string line;
+
+    // reading the file StudentData.txt
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string First_Name, Last_Name;
+        if (getline(ss, First_Name, ',') && getline(ss, Last_Name)) {
+            STUDENT_DATA student = { First_Name, Last_Name};
+            Students.push_back(student);
+        }
+    }
+
+    return 0;
 }
+
