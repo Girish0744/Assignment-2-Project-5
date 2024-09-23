bc2c094f34b60967532856b2f0f125ff8900047c
Girish0744 <girishbhuteja07@gmail.com>
2024-09-23 17:02:29 -0400

    Updated the code and added the functionality of pre release

--- Assignment 2 Project 5/Source.cpp
+++ Assignment 2 Project 5/Source.cpp
@@ -1,3 +1,76 @@
+//#include <iostream>
+//#include <fstream>
+//#include <vector>
+//#include <sstream>
+//
+//using namespace std;
+//
+//// Structure for the student data
+//struct STUDENT_DATA {
+//    string First_Name;
+//    string Last_Name;
+//    string Email;
+//};
+//
+//int main() {
+//
+//#ifdef PRE_RELEASE
+//    std::cout << "Pre-Release version.\n";
+//    std::ifstream file("StudentData_Emails.txt");
+//#else
+//    std::cout << "Standard version.\n";
+//    std::ifstream file("StudentData.txt");
+//#endif
+//
+//    vector<STUDENT_DATA> Students;
+//    string line;
+//
+//    // Opening the file
+//    if (file.is_open())
+//    {
+//        // Reading the file
+//        while (getline(file, line))
+//        {
+//            stringstream ss(line);
+//            string First_Name, Last_Name, Email;
+//
+//#ifdef PRE_RELEASE
+//            // In pre-release, we expect First_Name, Last_Name, and Email
+//            if (getline(ss, First_Name, ',') && getline(ss, Last_Name, ',') && getline(ss, Email)) {
+//                STUDENT_DATA student = { First_Name, Last_Name, Email };
+//#else
+//            // In standard, we only expect First_Name and Last_Name
+//            if (getline(ss, First_Name, ',') && getline(ss, Last_Name)) {
+//                STUDENT_DATA student = { First_Name, Last_Name, "" }; // No email in standard version
+//#endif
+//                Students.push_back(student);
+//            }
+//            }
+//        file.close();
+//        }
+//    else
+//    {
+//        cerr << "The file cannot be opened.\n";
+//        return 1;
+//    }
+//
+//    // Loop to print the student information
+//    for (const auto& student : Students)
+//    {
+//#ifdef PRE_RELEASE
+//        // Print First Name, Last Name, and Email
+//        cout << "First Name: " << student.First_Name << ", Last Name: " << student.Last_Name << ", Email: " << student.Email << endl;
+//#else
+//        // Only print First Name and Last Name
+//        cout << "First Name: " << student.First_Name << ", Last Name: " << student.Last_Name << endl;
+//#endif
+//    }
+//
+//    return 0;
+//    }
+//
+
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -13,7 +86,7 @@ struct STUDENT_DATA {
 };
 
 int main() {
-
+    // Checking the condition that we are in pre-release mode
 #ifdef PRE_RELEASE
     std::cout << "Pre-Release version.\n";
     std::ifstream file("StudentData_Emails.txt");
@@ -24,7 +97,7 @@ int main() {
 
     vector<STUDENT_DATA> Students;
     string line;
-
+ 
     // Opening the file
     if (file.is_open())
     {
@@ -38,34 +111,56 @@ int main() {
             // In pre-release, we expect First_Name, Last_Name, and Email
             if (getline(ss, First_Name, ',') && getline(ss, Last_Name, ',') && getline(ss, Email)) {
                 STUDENT_DATA student = { First_Name, Last_Name, Email };
+                Students.push_back(student);
+            }
 #else
             // In standard, we only expect First_Name and Last_Name
             if (getline(ss, First_Name, ',') && getline(ss, Last_Name)) {
                 STUDENT_DATA student = { First_Name, Last_Name, "" }; // No email in standard version
-#endif
                 Students.push_back(student);
             }
-            }
-        file.close();
+#endif
         }
+        file.close();
+    }
     else
     {
         cerr << "The file cannot be opened.\n";
         return 1;
     }
 
-    // Loop to print the student information
+    // To make sure if there is Debug or Release
+#ifdef _DEBUG
+    // Debug Mode
+#ifdef PRE_RELEASE
+        // Debug Pre-Release will print everything 
     for (const auto& student : Students)
     {
-#ifdef PRE_RELEASE
-        // Print First Name, Last Name, and Email
-        cout << "First Name: " << student.First_Name << ", Last Name: " << student.Last_Name << ", Email: " << student.Email << endl;
-#else
-        // Only print First Name and Last Name
-        cout << "First Name: " << student.First_Name << ", Last Name: " << student.Last_Name << endl;
-#endif
+        cout << "First Name: " << student.First_Name
+            << ", Last Name: " << student.Last_Name
+            << ", Email: " << student.Email << endl;
     }
+#else
+        // Debug Standard will print only the names
+    for (const auto& student : Students)
+    {
+        cout << "First Name: " << student.First_Name
+            << ", Last Name: " << student.Last_Name << endl;
+    }
+#endif
+#else
+    // Release Mode
+#ifdef PRE_RELEASE
+        // Release Pre-Release will print only emails
+    for (const auto& student : Students)
+    {
+        cout << "Email: " << student.Email << endl;
+    }
+#else
+        // Release Standard will print "Just running in Standard mode"
+    cout << "Just running in Standard mode.\n";
+#endif
+#endif
 
     return 0;
-    }
-
+}
